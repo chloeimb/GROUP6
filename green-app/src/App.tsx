e2272c09bcafeb3fde6d483819c68629c10a8cdc
Rohan <rohanvallapalli@gmail.com>
2023-11-12 18:55:02 -0500

    Links to Different Pages

--- green-app/src/App.tsx
+++ green-app/src/App.tsx
@@ -7,12 +7,18 @@ import {
 } from 'react-router-dom';
 import Login from './Login';
 import Home from './Home';
+import Profile from './Profile';
+import Analytics from './Analytics';
+import AddEntry from './AddEntry';
 
 const App: React.FC = () => (
   <Router>
     <Switch>
       <Route path="/login" exact component={Login} />
       <Route path="/home" exact component={Home} />
+      <Route path="/profile" exact component={Profile} />
+      <Route path="/analytics" exact component={Analytics} />
+      <Route path="/add-entry" exact component={AddEntry} />
       <Redirect from="/" to="/login" />
     </Switch>
   </Router>
---
+++ green-app/src/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import DenseAppBar from './Navigation/topbar';
+
+const Profile = () => {
+  return (
+    <div>
+      <DenseAppBar></DenseAppBar>
+      <h1>Profile</h1>
+    </div>
+  );
+};
+
+export default Profile;
