31402df3aaf436db8f58604544ef9ab318471657
chloeimbusch <107125600+chloeimbusch@users.noreply.github.com>
2023-11-18 15:21:38 -0500

    dashboard upload

--- green-app/src/Dashboard.js
+++ green-app/src/Dashboard.js
@@ -21,6 +21,7 @@ import { mainListItems, secondaryListItems } from './listItems';
 import Chart from './Chart';
 import Deposits from './Deposits';
 import Orders from './Orders';
+import TemporaryDrawer from './Navigation/sidebar';
 
 function Copyright(props) {
   return (
