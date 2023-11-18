2bc6e76cdf348e05a1182e35992864a0cb59ac66
chloeimbusch <107125600+chloeimbusch@users.noreply.github.com>
2023-11-18 09:57:15 -0500

    sign in side change

--- green-app/src/SignInSide.js
+++ green-app/src/SignInSide.js
@@ -12,6 +12,8 @@ import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import splashImage from './images/splashpage.png';
+
 
 function Copyright(props) {
   return (
@@ -50,7 +52,7 @@ export default function SignInSide() {
           sm={4}
           md={7}
           sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+            backgroundImage: `url(${splashImage})`,
             backgroundRepeat: 'no-repeat',
             backgroundColor: (t) =>
               t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
