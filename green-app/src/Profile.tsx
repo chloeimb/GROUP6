fe5e2d70729e540723d3567586ec59214a0521c5
chloeimbusch <107125600+chloeimbusch@users.noreply.github.com>
2023-11-25 12:13:34 -0500

    added bio age and gender

--- green-app/src/Profile.tsx
+++ green-app/src/Profile.tsx
@@ -8,23 +8,36 @@ import backgroundImage from './images/lightcolorbackground.png';
 type UserProfile = {
   name: string;
   email: string;
+  bio: string;
+  age: number;
+  gender: string;
   // Add other profile fields as needed
 };
 
 const Profile: React.FC = () => {
-  const [profile, setProfile] = useState<UserProfile>({ name: '', email: '' });
+  const [profile, setProfile] = useState<UserProfile>({
+    name: '',
+    email: '',
+    bio: '',
+    age: 0,
+    gender: '',
+  });
   const [profileImage, setProfileImage] = useState<File | null>(null);
   const [imagePreviewUrl, setImagePreviewUrl] = useState<string | null>(null);
   const [editMode, setEditMode] = useState(false);
 
-
-  // fetch user data when component mounts 
-  // replace with actual logic for fetching user data
-  // this is a dummy user object
   useEffect(() => {
-    setProfile({ name: 'John Doe', email: 'john@example.com'});
+    // Fetch user data when the component mounts
+    // You can replace this with your actual logic to fetch user data
+    // For now, I'm using a dummy user object
+    setProfile({
+      name: 'John Doe',
+      email: 'john@example.com',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      age: 25,
+      gender: 'Male',
+    });
   }, []);
-
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setProfile({ ...profile, [e.target.name]: e.target.value });
@@ -49,10 +62,11 @@ const Profile: React.FC = () => {
     const apiUrl = 'https://jsonplaceholder.typicode.com/posts';
 
     // Simulate a POST request to update user data
-    axios.post(apiUrl, { name: profile.name, email: profile.email })
+    axios
+      .post(apiUrl, { ...profile, image: profileImage })
       .then((response) => {
         console.log('User data updated successfully:', response.data);
-        setEditMode(false);
+        setEditMode(false); // Switch back to display mode after successful update
       })
       .catch((error) => {
         console.error('Error updating user data:', error);
@@ -61,17 +75,21 @@ const Profile: React.FC = () => {
 
   const containerStyle = {
     maxWidth: '500px',
-    margin: 'auto'
+    margin: 'auto',
   };
 
   return (
     <div style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center', height: '100vh' }}>
       <DenseAppBar />
+      <h1>Profile</h1>
       <div style={containerStyle}>
         {editMode ? (
           <form onSubmit={handleSubmit}>
             <input type="text" name="name" placeholder="Name" value={profile.name} onChange={handleInputChange} />
             <input type="email" name="email" placeholder="Email" value={profile.email} onChange={handleInputChange} />
+            <input type="text" name="bio" placeholder="Bio" value={profile.bio} onChange={handleInputChange} />
+            <input type="number" name="age" placeholder="Age" value={profile.age} onChange={handleInputChange} />
+            <input type="text" name="gender" placeholder="Gender" value={profile.gender} onChange={handleInputChange} />
             <input type="file" onChange={handleImageChange} />
             <button type="submit">Update Profile</button>
           </form>
@@ -81,6 +99,9 @@ const Profile: React.FC = () => {
             <div>
               <p><strong>Name:</strong> {profile.name}</p>
               <p><strong>Email:</strong> {profile.email}</p>
+              <p><strong>Bio:</strong> {profile.bio}</p>
+              <p><strong>Age:</strong> {profile.age}</p>
+              <p><strong>Gender:</strong> {profile.gender}</p>
             </div>
             <button onClick={() => setEditMode(true)}>Edit</button>
           </>
